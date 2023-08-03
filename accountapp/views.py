7bb699e5740e88e0fca0931ccc734ecc5fe15f1e
jaemoon <woans5970@naver.com>
2023-08-03 17:22:33 +0900

    Create DetailView

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from accountapp.models import HelloWorld
 from django.urls import reverse, reverse_lazy
 
@@ -30,3 +30,8 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
