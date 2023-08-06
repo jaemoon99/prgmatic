542171cfb03d9167a7f2c2bb00a1c8eb940ce3fc
jaemoon <woans5970@naver.com>
2023-08-06 12:48:50 +0900

    Setting MultipleObjectMixin

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,11 +5,14 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidde
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 from django.urls import reverse, reverse_lazy
+
+from articleapp.models import Article
 
 has_ownership = [
     account_ownership_required, login_required
@@ -40,10 +43,15 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
--- projectapp/urls.py
+++ projectapp/urls.py
@@ -7,5 +7,5 @@ app_name = 'projectapp'
 urlpatterns = [
     path('list/', ProjectListView.as_view(), name='list'),
     path('create/', ProjectCreateView.as_view(), name='create'),
-    path('detail/<int:pk>/', ProjectDetailView.as_view(), name='detail'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
 ]
