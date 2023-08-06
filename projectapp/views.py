d5945784a62520f55a3a7a29830e31115445b091
jaemoon <woans5970@naver.com>
2023-08-06 15:51:43 +0900

    Create Subscribeapp

--- projectapp/views.py
+++ projectapp/views.py
@@ -9,6 +9,8 @@ from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 from django.urls import reverse
 
+from subscribeapp.models import Subscription
+
 
 # Create your views here.
 @method_decorator(login_required, 'get')
@@ -28,8 +30,13 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
+        project = self.object
+        user = self.request.user
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+
         object_list = Article.objects.filter(project=self.get_object())
-        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, subscription=subscription,  **kwargs)
 
 class ProjectListView(ListView):
     model = Project
